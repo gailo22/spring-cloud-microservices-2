7feb7c9f3d2611de4ab700c87a285dbf2c52cf55
gailo22 <gailo22@gmail.com>
2018-03-03 15:42:48 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-cloud-microservices-2
 Based on spring boot 2.0
+
+## Eureka server
+```
+http://localhost:8761/
+```
---
+++ price-service/src/main/java/com/gailo22/microservices/priceservice/PriceServiceApplication.java
@@ -0,0 +1,14 @@
+package com.gailo22.microservices.priceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceServiceApplication.class, args);
+	}
+}
